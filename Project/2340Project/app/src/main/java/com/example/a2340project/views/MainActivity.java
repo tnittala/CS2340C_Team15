7bbf15682c918ef6dffa5770f68991159d2ae8cf
tnittala <tnittala@gmail.com>
2024-10-04 17:11:38 -0400

    login page, home page, started nav bar

--- Project/2340Project/app/src/main/java/com/example/a2340project/views/MainActivity.java
+++ Project/2340Project/app/src/main/java/com/example/a2340project/views/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.a2340project.views;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,6 +24,15 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        Button loginButton = findViewById(R.id.main_button);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
