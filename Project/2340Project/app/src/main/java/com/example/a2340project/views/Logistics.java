c48fdb95af1c74db573b89c0d3ba6a82d4940f02
tnittala <tnittala@gmail.com>
2024-10-06 17:24:18 -0400

    partial nav bar

--- Project/2340Project/app/src/main/java/com/example/a2340project/views/Logistics.java
+++ Project/2340Project/app/src/main/java/com/example/a2340project/views/Logistics.java
@@ -10,13 +10,13 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.example.a2340project.R;
 
-public class Destination extends AppCompatActivity {
+public class Logistics extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_destination);
+        setContentView(R.layout.activity_logistics);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
