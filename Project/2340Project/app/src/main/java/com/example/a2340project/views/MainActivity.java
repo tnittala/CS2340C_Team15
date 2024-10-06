7eeac750f560b4e46dbf784d4f78997275445f06
sritha8 <sritha.bhupatiraju@gmail.com>
2024-10-06 01:39:19 -0400

    adding the MainActivity.java file for WelcomePage code, as well as the altered AndroidManifest.xml

--- Project/2340Project/app/src/main/java/com/example/a2340project/views/MainActivity.java
+++ Project/2340Project/app/src/main/java/com/example/a2340project/views/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.a2340project.views;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,10 +20,34 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        Button startButton = findViewById(R.id.startButton);
+        Button quitButton = findViewById(R.id.quitButton);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        //handle start button
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //open a new activity or main functionality
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class); //Intent directs this activity to the next activity
+                startActivity(intent);
+            }
+        });
+
+        //handle quit button
+        quitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //quit app
+                finish();
+                System.exit(0);
+            }
         });
     }
 }
