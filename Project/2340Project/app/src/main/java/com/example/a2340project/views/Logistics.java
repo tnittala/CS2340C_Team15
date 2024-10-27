6bace04afd06ebeac5a5e3973ed7d965d1265896
jkaur77 <1kjapleen@gmail.com>
2024-10-27 19:58:40 -0400

    updated logistics to add graph

--- Project/2340Project/app/src/main/java/com/example/a2340project/views/Logistics.java
+++ Project/2340Project/app/src/main/java/com/example/a2340project/views/Logistics.java
@@ -1,9 +1,11 @@
 package com.example.a2340project.views;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
@@ -14,10 +16,31 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.a2340project.R;
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
+import java.util.ArrayList;
+
 public class Logistics extends AppCompatActivity {
+
+    private BarChart barChart;
+
+    private void graphTrips() {
+        ArrayList<BarEntry> entries = new ArrayList<>();
+        entries.add(new BarEntry(0, 4));
+        entries.add(new BarEntry(1, 3));
+
+        BarDataSet dataSet = new BarDataSet(entries, "Trip Days");
+        dataSet.setColors(Color.RED, Color.BLUE);
+        BarData barData = new BarData(dataSet);
+        barChart.setData(barData);
+        barChart.invalidate(); //this refreshes the chart
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +51,16 @@ public class Logistics extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        Button graphButton = findViewById(R.id.button_tripgraph);
+        barChart = findViewById(R.id.barChart);
+
+        graphButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                graphTrips();
+            }
         });
 
         // home button
