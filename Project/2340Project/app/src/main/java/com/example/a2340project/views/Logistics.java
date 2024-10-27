449829a89e1ad20bb9833022c0c45e60f7c37fc4
jkaur77 <1kjapleen@gmail.com>
2024-10-27 14:37:52 -0400

    added code for graph

--- Project/2340Project/app/src/main/java/com/example/a2340project/views/Logistics.java
+++ Project/2340Project/app/src/main/java/com/example/a2340project/views/Logistics.java
@@ -14,6 +14,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.a2340project.R;
+import com.github.mikephil.charting.charts.BarChart;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
@@ -29,6 +30,9 @@ public class Logistics extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        BarChart barchart = findViewById(R.id.barChart);
+        
 
         // home button
         ImageButton homeBtn = findViewById(R.id.homeButton);
