a88343117b38132b1a54ac3cab2fd8831e927d32
tnittala <tnittala@gmail.com>
2024-10-27 22:45:54 -0400

    singleton test

--- Project/2340Project/app/src/test/java/com/example/a2340project/ExampleUnitTest.java
+++ Project/2340Project/app/src/test/java/com/example/a2340project/ExampleUnitTest.java
@@ -1,17 +1,27 @@
 package com.example.a2340project;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import static org.junit.Assert.*;
+
+import com.example.a2340project.model.FirebaseDatabaseSingleton;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
+//@RunWith(AndroidJUnit4.class)
 public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+
+        FirebaseDatabaseSingleton instance1 = FirebaseDatabaseSingleton.getInstance();
+        FirebaseDatabaseSingleton instance2 = FirebaseDatabaseSingleton.getInstance();
+
+        // Check if both instances are the same
+        assertEquals(instance1, instance2);
+
     }
 }
